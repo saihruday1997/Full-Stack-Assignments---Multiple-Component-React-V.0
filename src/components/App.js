21099025109d8b425d64a3525b4083c33ba892c3
Hruday Bharadwaj <saihruday.08@gmail.com>
2020-10-19 00:13:20 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <p data-ns-test="project-name">Name of the Project</p>
+        <p data-ns-test="project-description">Description of the Project</p>
+      </>
+    );
+  }
 }
-
 
 export default App;
